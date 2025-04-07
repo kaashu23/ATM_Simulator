0d98cacf6cb8f752e1cc5f46612a5bfdbd33c8f5
Kashish Salvi <161292851+kaashu23@users.noreply.github.com>
2025-04-07 22:05:39 +0530

    Delete ATM_Simulator directory

--- ATM_Simulator/src/atm_simulator/ATM_Simulator.java
+++
@@ -1,7 +0,0 @@
-package atm_simulator;
-public class ATM_Simulator {
-    public static void main(String[] args) {
-        System.out.print("Hello");
-        Dashboard d = new Dashboard();
-    }
-}
